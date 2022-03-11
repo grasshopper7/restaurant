139117f790be8be61761db31b45fe2d100127d4a
grasshopper7 <grass.hopper.moc@gmail.com>
2022-03-11 18:38:53 +0530

    Added extent report adapter

--- restaurant/src/test/java/ristorante/CukeRunnerIT.java
+++ restaurant/src/test/java/ristorante/CukeRunnerIT.java
@@ -6,5 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin = { "summary", "json:target/report/cucumber.json" })
-public class CukeRunnerIT {}
+@CucumberOptions(plugin = { "summary", "json:target/report/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+public class CukeRunnerIT {
+}
